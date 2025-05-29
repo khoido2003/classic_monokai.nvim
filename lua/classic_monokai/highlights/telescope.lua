a483618d1a1f63fca8fb2be9e769958dec888bbb
khoido2003 <khoizpro31@gmail.com>
2025-05-30 01:01:56 +0700

    Fix: telesope UI color

--- lua/classic_monokai/highlights/telescope.lua
+++ lua/classic_monokai/highlights/telescope.lua
@@ -11,8 +11,6 @@ function M.get(c)
     TelescopePromptPrefix = { fg = c.blue_alt },
     TelescopeSelection = { fg = c.fg, bg = c.bg_statusline },
     TelescopeSelectionCaret = { fg = c.magenta, bg = c.bg_statusline },
-    TelescopePromptNormal = { bg = c.bg_dark },
-    TelescopeResultsTitle = { fg = c.fg, bg = c.bg_dark },
     TelescopePromptCounter = { fg = c.blue_alt },
     TelescopeMatching = { fg = c.green, bold = true },
   }
