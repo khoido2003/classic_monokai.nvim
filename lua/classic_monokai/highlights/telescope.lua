070a2d7bc6901824fc56d89ae659cd194e175408
khoido2003 <khoizpro31@gmail.com>
2025-05-31 15:35:24 +0700

    fix(telescope): unify prompt background colors for consistent UI

--- lua/classic_monokai/highlights/telescope.lua
+++ lua/classic_monokai/highlights/telescope.lua
@@ -5,13 +5,16 @@ M.url = "https://github.com/nvim-telescope/telescope.nvim"
 ---@type monokai.HighlightsFn
 function M.get(c)
   return {
-    TelescopeBorder = "FloatBorder",
     TelescopeNormal = { fg = c.grey_light, bg = c.bg_float },
+    TelescopeBorder = { fg = c.border, bg = c.bg_float },
+    TelescopePromptNormal = { fg = c.fg, bg = c.bg_float },
+    TelescopePromptBorder = { fg = c.border, bg = c.bg_float },
+    TelescopePromptTitle = { fg = c.yellow, bg = c.bg_float, bold = true },
+    TelescopePromptPrefix = { fg = c.blue_alt, bg = c.bg_float },
+    TelescopePromptCounter = { fg = c.green, bg = c.bg_float },
     TelescopePreviewLine = { bg = c.bg_statusline },
-    TelescopePromptPrefix = { fg = c.blue_alt },
     TelescopeSelection = { fg = c.fg, bg = c.bg_statusline },
     TelescopeSelectionCaret = { fg = c.magenta, bg = c.bg_statusline },
-    TelescopePromptCounter = { fg = c.green, bg = c.bg_float },
     TelescopeMatching = { fg = c.green, bold = true },
   }
 end
