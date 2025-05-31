0ca3d77f01361665190c5d6410db5b1824999430
khoido2003 <khoizpro31@gmail.com>
2025-05-31 15:47:57 +0700

    feat(telescope): align highlight groups with Monokai palette

--- lua/classic_monokai/highlights/telescope.lua
+++ lua/classic_monokai/highlights/telescope.lua
@@ -3,25 +3,14 @@ local M = {}
 M.url = "https://github.com/nvim-telescope/telescope.nvim"
 
 ---@type monokai.HighlightsFn
-function M.get(c)
+function M.get(c, opts)
+  -- stylua: ignore
   return {
-    TelescopeNormal         = { fg = c.fg, bg = c.bg_float },
-    TelescopeBorder         = { fg = c.border, bg = c.bg_float },
-    TelescopePromptNormal   = { fg = c.fg, bg = c.bg_float },
-    TelescopePromptBorder   = { fg = c.border, bg = c.bg_float },
-    TelescopePromptTitle    = { fg = c.yellow, bg = c.bg_float, bold = true },
-    TelescopePromptPrefix   = { fg = c.blue_alt, bg = c.bg_float },
-    TelescopePromptCounter  = { fg = c.green, bg = c.bg_float },
-    TelescopeResultsNormal  = { fg = c.fg, bg = c.bg_float },
-    TelescopeResultsBorder  = { fg = c.border, bg = c.bg_float },
-    TelescopeResultsTitle   = { fg = c.yellow, bg = c.bg_float, bold = true },
-    TelescopePreviewNormal  = { fg = c.fg, bg = c.bg_float },
-    TelescopePreviewBorder  = { fg = c.border, bg = c.bg_float },
-    TelescopePreviewTitle   = { fg = c.yellow, bg = c.bg_float, bold = true },
-    TelescopePreviewLine    = { bg = c.bg_statusline },
-    TelescopeSelection      = { fg = c.fg, bg = c.bg_statusline },
-    TelescopeSelectionCaret = { fg = c.magenta, bg = c.bg_statusline },
-    TelescopeMatching       = { fg = c.green, bold = true },
+    TelescopeBorder         = { fg = c.charcoal_light or c.grey_dark, bg = c.bg_float or c.bg_dark or c.bg },
+    TelescopeNormal         = { fg = c.fg, bg = c.bg_float or c.bg_dark or c.bg },
+    TelescopePromptBorder   = { fg = c.orange, bg = c.bg_float or c.bg_dark or c.bg },
+    TelescopePromptTitle    = { fg = c.orange, bg = c.bg_float or c.bg_dark or c.bg },
+    TelescopeResultsComment = { fg = c.grey_medium or c.comment },
   }
 end
 
